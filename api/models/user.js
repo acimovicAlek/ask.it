1b70cb08853aa3d514f3cc4aaf019ce691d7b0eb
acimovicAlek <acimovic.alek@gmail.com>
2019-12-20 10:49:15 +0100

    Add some validation

--- api/models/user.js
+++ api/models/user.js
@@ -4,7 +4,9 @@ const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    dropDups: true
   },
   password: {
     type: String,
